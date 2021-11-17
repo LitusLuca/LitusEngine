8962516129cd0325b6b7567229829bed4b63a3db
Luca <wachters.luca@gmail.com>
2021-11-17 16:43:03 +0100

    Added blending

--- LitusEngine/src/Platform/OpenGL/OpenGLRenderAPI.cpp
+++ LitusEngine/src/Platform/OpenGL/OpenGLRenderAPI.cpp
@@ -32,7 +32,9 @@ namespace LT {
 
 		glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_NOTIFICATION, 0, NULL, GL_FALSE);
 		#endif // LT_DEBUG
-		//TODO Blending
+		
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 		glEnable(GL_DEPTH_TEST);
 	}
